6fbaf7f8cfeb03d88047b7ec1ae96574afd91713
sunghankwon <akvo852@naver.com>
2024-03-23 17:30:43 +0900

    fix: Handle edge cases in quotation conversion with flexible regex

--- src/utils/transformQuotesInMarkdown.js
+++ src/utils/transformQuotesInMarkdown.js
@@ -1,9 +1,9 @@
 async function transformQuotesInMarkdown(markdown) {
-  let markdownSentences = markdown.split(`\n`);
-  let transformedArray = markdownSentences.map((str) => {
-    const pattern = /\*\*<u>\((.*?)#\)<\/u>\*\*/g;
-    let transformedStr = str.replace(pattern, (p1) => {
-      let cleanedQuote = p1.replace(/(\*\*|_|<u>|<\/u>)/g, "");
+  const markdownSentences = markdown.split(`\n`);
+  const transformedArray = markdownSentences.map((str) => {
+    const pattern = /(?:\*\*)?(?:<u>)?\((.*?)#\)(?:<\/u>)?(?:\*\*)?/g;
+    const transformedStr = str.replace(pattern, (p1) => {
+      const cleanedQuote = p1.replace(/(\*\*|_|<u>|<\/u>)/g, "");
 
       return `> ${cleanedQuote}`;
     });
