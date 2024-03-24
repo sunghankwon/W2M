de13759f758059a2322badf3fda2138245d22c19
sunghankwon <akvo852@naver.com>
2024-03-24 20:55:14 +0900

    feat: Added details to the bold button in the UI

--- src/components/CustomTools/BoldButton.jsx
+++ src/components/CustomTools/BoldButton.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import boldIcon from "../../assets/Bold.png";
 
 export function BoldButton({
@@ -8,23 +9,48 @@ export function BoldButton({
 }) {
   const applyBold = () => {
     const textarea = editorRef.current;
-    const startPos = textarea.selectionStart;
-    const endPos = textarea.selectionEnd;
+    let startPos = textarea.selectionStart;
+    let endPos = textarea.selectionEnd;
+    const textBefore = markdownText.substring(0, startPos);
+    const textAfter = markdownText.substring(endPos);
     const selectedText = markdownText.substring(startPos, endPos);
 
     let newText;
 
-    if (selectedText) {
+    const hasBoldBefore = textBefore.endsWith("**");
+    const hasBoldAfter = textAfter.startsWith("**");
+
+    if (hasBoldBefore && hasBoldAfter && selectedText) {
       newText =
-        markdownText.substring(0, startPos) +
-        `**${selectedText}**` +
-        markdownText.substring(endPos);
+        markdownText.substring(0, startPos - 2) +
+        selectedText +
+        markdownText.substring(endPos + 2);
+      startPos -= 2;
+      endPos -= 2;
+    } else if (!hasBoldBefore && !hasBoldAfter && selectedText) {
+      if (
+        selectedText.startsWith("**") &&
+        selectedText.endsWith("**") &&
+        selectedText.length > 4
+      ) {
+        const trimmedText = selectedText.substring(2, selectedText.length - 2);
+        newText =
+          markdownText.substring(0, startPos) +
+          trimmedText +
+          markdownText.substring(endPos);
+      } else {
+        newText =
+          markdownText.substring(0, startPos) +
+          `**${selectedText}**` +
+          markdownText.substring(endPos);
+      }
     } else {
       newText = `${markdownText.substring(0, startPos)}****${markdownText.substring(startPos)}`;
       setTimeout(() => setCursorPosition(startPos + 2), 0);
     }
 
     setMarkdownText(newText);
+    textarea.setSelectionRange(startPos, endPos);
     textarea.focus();
   };
 
@@ -33,7 +59,7 @@ export function BoldButton({
       onClick={applyBold}
       className="p-2 border rounded-lg hover:bg-gray-200"
     >
-      <img src={boldIcon} className="h-5"></img>
+      <img src={boldIcon} alt="Bold" className="h-5" />
     </button>
   );
 }
