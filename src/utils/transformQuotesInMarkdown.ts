1252c10315f8fe9c68a154c858ce3706b2112fe7
sunghankwon <akvo852@naver.com>
2024-06-11 02:45:23 +0900

    refactor: migrate transformQuotesInMarkdown utility function to TypeScript

--- src/utils/transformQuotesInMarkdown.ts
+++ src/utils/transformQuotesInMarkdown.ts
@@ -1,13 +1,11 @@
-async function transformQuotesInMarkdown(markdown) {
+async function transformQuotesInMarkdown(markdown: string): Promise<string> {
   const markdownSentences = markdown.split(`\n`);
   const transformedArray = markdownSentences.map((str) => {
     const pattern = /(?:\*\*)?(?:<u>)?\((.*?)#\)(?:<\/u>)?(?:\*\*)?/g;
-    const transformedStr = str.replace(pattern, (p1) => {
+    const transformedStr = str.replace(pattern, (match, p1) => {
       const cleanedQuote = p1.replace(/(\*\*|_|<u>|<\/u>)/g, "");
-
       return `> ${cleanedQuote}`;
     });
-
     return transformedStr;
   });
 
