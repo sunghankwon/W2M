a744faabdf2fd5ff00fbc8c16126dca63b55008e
sunghankwon <akvo852@naver.com>
2024-07-01 21:10:05 +0900

    refactor: Migrate processChildNodes to TypeScript

--- src/utils/processChildNodes.ts
+++ src/utils/processChildNodes.ts
@@ -1,18 +1,28 @@
 import printTextNodes from "./printTextNodes";
 
+interface DocxFilesData {
+  [key: string]: string;
+}
+
+interface ListItemCounters {
+  [numId: string]: {
+    [ilvl: string]: number;
+  };
+}
+
 async function processChildNodes(
-  node,
-  docxFilesData,
-  markdown,
-  depth,
-  headingLevel,
-  numberingLevel,
-  markdownSyntax,
-  listItemCounters,
-  isListItem,
-  processedImages,
-  addedImages,
-) {
+  node: ChildNode,
+  docxFilesData: DocxFilesData,
+  markdown: string,
+  depth: number,
+  headingLevel: string,
+  numberingLevel: string,
+  markdownSyntax: string,
+  listItemCounters: ListItemCounters,
+  isListItem: boolean,
+  processedImages: Set<string>,
+  addedImages: Set<string>,
+): Promise<string> {
   for (const child of Array.from(node.childNodes)) {
     const childMarkdown = await printTextNodes(
       child,
