9daaeacb48386c2f14eba80a4c9bf8422002907f
sunghankwon <akvo852@naver.com>
2024-07-01 21:35:53 +0900

    refactor: Migrate Preview to TypeScript

---
+++ src/components/Preview/index.tsx
@@ -0,0 +1,33 @@
+import { forwardRef, UIEvent } from "react";
+import useImagePathsUpdater from "../../hooks/useImagePathsUpdater";
+import useFullScreenStore from "../../store/useFullScreen";
+import getMarkdownRenderer from "../../utils/getMarkdownRenderer";
+import getMarkdownText from "../../utils/getMarkdownText";
+
+interface PreviewProps {
+  handlePreviewScroll: (event: UIEvent<HTMLDivElement>) => void;
+}
+
+const Preview = forwardRef<HTMLDivElement, PreviewProps>(
+  ({ handlePreviewScroll }, ref) => {
+    const { isFullScreen } = useFullScreenStore();
+    const processedMarkdown = useImagePathsUpdater();
+
+    const renderer = getMarkdownRenderer();
+
+    return (
+      <div
+        ref={ref}
+        onScroll={handlePreviewScroll}
+        className={`w-full max-w-[800px] min-w-[706px] h-auto min-h-[650px] border rounded-md overflow-y-auto scrollbar-hide pl-2 prose lg:prose-xs markdown-preview ${
+          !isFullScreen ? "max-h-[650px]" : ""
+        }`}
+        dangerouslySetInnerHTML={getMarkdownText(processedMarkdown, renderer)}
+      />
+    );
+  },
+);
+
+Preview.displayName = "Preview";
+
+export default Preview;
