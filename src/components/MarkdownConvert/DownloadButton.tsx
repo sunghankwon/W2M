c5d0e7cc1e0fec328b22166bd65e453d6434ff81
sunghankwon <akvo852@naver.com>
2024-07-02 19:21:15 +0900

    refactor: Migrate DownloadButton to TypeScript

--- src/components/MarkdownConvert/DownloadButton.tsx
+++ src/components/MarkdownConvert/DownloadButton.tsx
@@ -2,7 +2,17 @@ import JSZip from "jszip";
 import { saveAs } from "file-saver";
 import downloadIcon from "../../assets/download.png";
 
-const DownloadButton = ({ originName, markdownText, docxFilesData }) => {
+interface DownloadButtonProps {
+  originName: string;
+  markdownText: string;
+  docxFilesData: Record<string, string>;
+}
+
+const DownloadButton = ({
+  originName,
+  markdownText,
+  docxFilesData,
+}: DownloadButtonProps) => {
   const downloadMarkdownFile = async () => {
     const hasImages = Object.keys(docxFilesData).some((key) =>
       key.startsWith("word/media/"),
@@ -30,7 +40,7 @@ const DownloadButton = ({ originName, markdownText, docxFilesData }) => {
       onClick={downloadMarkdownFile}
       className="flex items-center p-2 mr-2 rounded-lg hover:bg-gray-400"
     >
-      <img src={downloadIcon} className="h-5 mr-2" />
+      <img src={downloadIcon} className="h-5 mr-2" alt="Download Icon" />
       Download
     </button>
   );
