e2a81936b31f994c7bad054964f3745efd276df9
sunghankwon <akvo852@naver.com>
2024-07-02 19:20:58 +0900

    refactor: Migrate CopyButton to TypeScript

--- src/components/MarkdownConvert/CopyButton.tsx
+++ src/components/MarkdownConvert/CopyButton.tsx
@@ -1,13 +1,21 @@
 import copyIcon from "../../assets/copy.png";
 import checkIcon from "../../assets/check.png";
 
+interface CopyButtonProps {
+  markdownText: string;
+  buttonText: string;
+  setButtonText: React.Dispatch<React.SetStateAction<string>>;
+  iconSrc: string;
+  setIconSrc: React.Dispatch<React.SetStateAction<string>>;
+}
+
 const CopyButton = ({
   markdownText,
   buttonText,
   setButtonText,
   iconSrc,
   setIconSrc,
-}) => {
+}: CopyButtonProps) => {
   const copyToClipboard = () => {
     navigator.clipboard
       .writeText(markdownText)
@@ -30,7 +38,7 @@ const CopyButton = ({
       onClick={copyToClipboard}
       className="flex items-center p-2 rounded-lg hover:bg-stone-600"
     >
-      <img src={iconSrc} className="h-5 mr-2" />
+      <img src={iconSrc} className="h-5 mr-2" alt="Copy Icon" />
       {buttonText}
     </button>
   );
