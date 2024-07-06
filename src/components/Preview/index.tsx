8a8b66557abf59239d2536f20217279cd9b88d8b
sunghankwon <akvo852@naver.com>
2024-07-06 22:14:58 +0900

    refactor: Update Preview component to handle asynchronous markdown rendering

--- src/components/Preview/index.tsx
+++ src/components/Preview/index.tsx
@@ -1,7 +1,6 @@
-import { forwardRef, UIEvent } from "react";
+import { forwardRef, UIEvent, useEffect, useState } from "react";
 import useImagePathsUpdater from "../../hooks/useImagePathsUpdater";
 import useFullScreenStore from "../../store/useFullScreen";
-import getMarkdownRenderer from "../../utils/getMarkdownRenderer";
 import getMarkdownText from "../../utils/getMarkdownText";
 
 interface PreviewProps {
@@ -12,8 +11,18 @@ const Preview = forwardRef<HTMLDivElement, PreviewProps>(
   ({ handlePreviewScroll }, ref) => {
     const { isFullScreen } = useFullScreenStore();
     const processedMarkdown = useImagePathsUpdater();
+    const [htmlContent, setHtmlContent] = useState<{ __html: string }>({
+      __html: "",
+    });
 
-    const renderer = getMarkdownRenderer();
+    useEffect(() => {
+      const processMarkdown = async () => {
+        const content = await getMarkdownText(processedMarkdown);
+        setHtmlContent(content);
+      };
+
+      processMarkdown();
+    }, [processedMarkdown]);
 
     return (
       <div
@@ -22,7 +31,7 @@ const Preview = forwardRef<HTMLDivElement, PreviewProps>(
         className={`w-full max-w-[800px] min-w-[706px] h-auto min-h-[650px] border rounded-md overflow-y-auto scrollbar-hide pl-2 prose lg:prose-xs markdown-preview ${
           !isFullScreen ? "max-h-[650px]" : ""
         }`}
-        dangerouslySetInnerHTML={getMarkdownText(processedMarkdown, renderer)}
+        dangerouslySetInnerHTML={htmlContent}
       />
     );
   },
