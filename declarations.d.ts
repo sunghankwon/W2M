e74510fb9815da2a447fd54f6abcb3002230e1ea
sunghankwon <akvo852@naver.com>
2024-07-06 22:03:06 +0900

    chore: Add custom type declarations for 'file-saver' and image files in declarations.d.ts

---
+++ declarations.d.ts
@@ -0,0 +1,28 @@
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.jpg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.jpeg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.gif" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.svg" {
+  const value: string;
+  export default value;
+}
+
+declare module "file-saver" {
+  export function saveAs(data: Blob, filename: string): void;
+}
